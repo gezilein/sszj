22201355121a3aa5fb3032705bf1b0099dc3e238
Matej Fartelj <matej.fartelj@solve-x.net>
2023-01-20 21:11:16 +0100

    Output route param (POC)

--- src/components/Lesson.vue
+++ src/components/Lesson.vue
@@ -1,8 +1,12 @@
 <script setup lang="ts">
+import { useRoute } from "vue-router";
 import { useCourseStore } from "../store/CourseStore";
 const store = useCourseStore();
+const route = useRoute();
 </script>
 
 <template>
   <h2>Poglavje {{ store.courses.length }}</h2>
+  {{ route.fullPath }}
+  {{ route.params.course }}
 </template>
