17b60f965ad99f00147c69f2bc29951f249a795b
Matej Fartelj <matej.fartelj@solve-x.net>
2023-01-20 20:17:52 +0100

    Include store in component

---
+++ src/components/Lesson.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+import { useCourseStore } from "../store/CourseStore";
+const store = useCourseStore();
+</script>
+
+<template>
+  <h2>Poglavje {{ store.courses.length }}</h2>
+</template>
--- src/views/Workbook.vue
+++ src/views/Workbook.vue
@@ -1,6 +1,8 @@
 <template>
   <h1>Učbenik</h1>
+  <Lesson></Lesson>
 </template>
 
 <script setup lang="ts">
+import Lesson from '../components/Lesson.vue';
 </script>
