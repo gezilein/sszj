f41e471f554863fea4d28bfc648d99bbcbcc9f0d
Matej Fartelj <matej.fartelj@solve-x.net>
2023-01-20 20:17:32 +0100

    Use PanelMenu for navigation

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,15 +1,14 @@
 
 <script setup lang="ts">
-import Menu from "primevue/menu";
-import { MenuItem } from "primevue/menuitem";
+import PanelMenu from 'primevue/panelmenu';
 import { PrimeIcons } from "primevue/api";
 
-let items: MenuItem[] = [
+let items = [
   { label: "Abeceda", to: "/abeceda", icon: PrimeIcons.THUMBS_UP_FILL },
   { label: "Učbenik", to: "/ucbenik", icon: PrimeIcons.BOOK },
 ];
 </script>
 
 <template>
-  <Menu :model="items" position="left" />
+  <PanelMenu :model="items" />
 </template>
