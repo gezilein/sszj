192f3f1ea44d1d5fea8c75d6a852d8f1f38d17ef
Matej Fartelj <matej.fartelj@solve-x.net>
2023-01-20 21:10:52 +0100

    Build navigation dynamically

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,12 +1,22 @@
-
 <script setup lang="ts">
-import PanelMenu from 'primevue/panelmenu';
+import PanelMenu from "primevue/panelmenu";
 import { PrimeIcons } from "primevue/api";
+import { useCourseStore } from "../store/CourseStore";
+import { onBeforeMount, onMounted } from "vue-demi";
+import { MenuItem } from "primevue/menuitem/MenuItem";
+import { useRoute, useRouter } from "vue-router";
+import Workbook from "../views/Workbook.vue";
 
-let items = [
-  { label: "Abeceda", to: "/abeceda", icon: PrimeIcons.THUMBS_UP_FILL },
-  { label: "Učbenik", to: "/ucbenik", icon: PrimeIcons.BOOK },
-];
+let items: MenuItem[] = [{ label: "Abeceda", to: "/abeceda", icon: PrimeIcons.THUMBS_UP_FILL }];
+const router = useRouter();
+
+onBeforeMount(() => {
+  const store = useCourseStore();
+  store.getCourses.forEach((c) => {
+    items.push({ label: c.title, to: "/" + c.slug });
+    // router.addRoute({ path: "/ucbenik/" + c.slug, component: Workbook });
+  });
+});
 </script>
 
 <template>
