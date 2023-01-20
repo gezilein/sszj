4d160b704dc7af2717ea3d79129cb4b6cd51ec36
Matej Fartelj <matej.fartelj@solve-x.net>
2023-01-20 18:02:13 +0100

    Use Pinia with store

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,8 @@ import { createRouter, createWebHashHistory } from "vue-router";
 import Home from './views/Home.vue';
 import Alphabet from './views/Alphabet.vue';
 import Workbook from "./views/Workbook.vue";
+import { createPinia } from 'pinia'
+
 
 const router = createRouter({
     history: createWebHashHistory(),
@@ -15,7 +17,10 @@ const router = createRouter({
     ],
 })
 
+const pinia = createPinia()
+
 const app = createApp(App);
 app.use(router);
+app.use(pinia);
 app.use(PrimeVue);
 app.mount('#app');
