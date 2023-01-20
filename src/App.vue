930d5bd921dae5e1e8e0ede350b81c9deaed2e31
Matej Fartelj <matej.fartelj@solve-x.net>
2023-01-20 18:02:58 +0100

    Load static asset data POC

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,18 @@
 <script setup lang="ts">
+import { onMounted } from "vue";
 import Navigation from "./components/Navigation.vue";
+import { useLessonStore } from "./store/LessonStore";
+import lessonData from "./assets/lessons.json";
+
+onMounted(() => {
+  const store = useLessonStore();
+  console.log(lessonData);
+});
 </script>
 
 <template>
   <div class="grid">
-    <div class="col-fixed" style="width: 220px">
+    <div class="col-fixed">
       <Navigation />
     </div>
     <div class="col">
