da869b94ba3f4b5c7c629f775780ffaf4e36cdc2
Matej Fartelj <matej.fartelj@solve-x.net>
2023-01-21 14:11:45 +0100

    Place template after script

--- src/views/Workbook.vue
+++ src/views/Workbook.vue
@@ -1,8 +1,8 @@
+<script setup lang="ts">
+import Lesson from "../components/Lesson.vue";
+</script>
+
 <template>
   <h1>Učbenik</h1>
   <Lesson></Lesson>
 </template>
-
-<script setup lang="ts">
-import Lesson from '../components/Lesson.vue';
-</script>
